26ba569715e511e69a4efeb795c0beb49da01a35
Chris Seltzer <csmastermind@gmail.com>
2016-12-19 19:06:17 -0500

    Extract amorphicEntry

--- index.js
+++ index.js
@@ -83,12 +83,9 @@ reset();
 
 var establishApplication = require('./lib/establishApplication').establishApplication;
 var establishDaemon = require('./lib/establishDaemon').establishDaemon;
-var establishServerSession = require('./lib/establishServerSession').establishServerSession;
 var Utils = require('./lib/utils');
 var logMessage = Utils.logMessage;
 var getTemplates = require('./lib/getTemplates').getTemplates;
-var getModelSource = require('./lib/getModelSource').getModelSource;
-var getModelSourceMap = require('./lib/getModelSourceMap').getModelSourceMap;
 var router = require('./lib/router').router;
 var uploadRouter = require('./lib/uploadRouter').uploadRouter;
 var postRouter = require('./lib/postRouter').postRouter;
@@ -99,117 +96,11 @@ var intializePerformance = require('./lib/intializePerformance').intializePerfor
 var handleDBCase = require('./lib/handleDBCase').handleDBCase;
 var generateDownloadsDir = require('./lib/generateDownloadsDir').generateDownloadsDir;
 var fetchStartUpParams = require('./lib/fetchStartUpParams').fetchStartUpParams;
+var amorphicEntry = require('./lib/amorphicEntry').amorphicEntry;
 
 function localGetTemplates(objectTemplate, appPath, templates, config, path, sourceOnly, detailedInfo) {
     return getTemplates(objectTemplate, appPath, templates, config, path, sourceOnly, detailedInfo,
   amorphicOptions, applicationSource, applicationSourceMap, applicationPersistorProps);
-}
-
-/**
- * Purpose unknown
- *
- * @param {unknown} req unknown
- * @param {unknown} resp unknown
- * @param {unknown} next unknown
- */
-function amorphicEntry(req, resp, next) {
-    // If we're not initalizing
-    if (!req.url.match(/amorphic\/init/)) {
-        next();
-    }
-
-    logMessage('Requesting ' + req.originalUrl);
-
-    req.amorphicTracking.loggingContext.session = req.session.id;
-
-    req.amorphicTracking.loggingContext.ipaddress =
-        (String(req.headers['x-forwarded-for'] || req.connection.remoteAddress))
-            .split(',')[0].replace(/(.*)[:](.*)/, '$2') || 'unknown';
-
-    var time = process.hrtime();
-    var appName;
-
-    if (req.originalUrl.match(/([A-Za-z0-9_]*)\.cached.js.map/)) {
-        appName = RegExp.$1;
-
-        req.amorphicTracking.loggingContext.app = appName;
-        resp.setHeader('Content-Type', 'application/javascript');
-        resp.setHeader('Cache-Control', 'public, max-age=31556926');
-        resp.end(getModelSourceMap(appName, applicationSourceMap));
-
-        req.amorphicTracking.addServerTask({name: 'Request Source Map'}, time);
-        displayPerformance(req);
-    }
-    else if (req.originalUrl.match(/([A-Za-z0-9_]*)\.cached.js/)) {
-        appName = RegExp.$1;
-
-        req.amorphicTracking.loggingContext.app = appName;
-        resp.setHeader('Content-Type', 'application/javascript');
-        resp.setHeader('Cache-Control', 'public, max-age=31556926');
-
-        if (amorphicOptions.sourceMode == 'prod') {
-            if (req.originalUrl.match(/(\?ver=[0-9]+)/)) {
-                resp.setHeader('X-SourceMap', '/amorphic/init/' + appName + '.cached.js.map?ver=' + RegExp.$1);
-            }
-            else {
-                resp.setHeader('X-SourceMap', '/amorphic/init/' + appName + '.cached.js.map?ver=');
-            }
-        }
-
-        resp.end(getModelSource(appName, applicationSource));
-
-        req.amorphicTracking.addServerTask('Request Compressed Sources', time);
-        displayPerformance(req);
-    }
-    else if (req.originalUrl.match(/([A-Za-z0-9_-]*)\.js/)) {
-        var url = req.originalUrl;
-        appName = RegExp.$1;
-
-        req.amorphicTracking.loggingContext.app = appName;
-        logMessage('Establishing ' + appName);
-
-        establishServerSession(req, appName, 'initial', false, null, applicationConfig, sessions, amorphicOptions, applicationSource, applicationSourceMap, applicationPersistorProps, hostName, controllers, nonObjTemplatelogLevel, sendToLog)
-            .then (function a(session) {
-                var time = process.hrtime();
-
-                if (req.method == 'POST' && session.objectTemplate.controller.processPost) {
-                    Q(session.objectTemplate.controller.processPost(req.originalUrl, req.body, req)).then(function b(controllerResp) {
-                        session.save(appName, req.session, req);
-                        resp.writeHead(controllerResp.status, controllerResp.headers || {'Content-Type': 'text/plain'});
-                        resp.end(controllerResp.body || '');
-                    });
-
-                    req.amorphicTracking.addServerTask({name: 'Application Post'}, time);
-                    displayPerformance(req);
-                }
-                else {
-                    resp.setHeader('Content-Type', 'application/javascript');
-                    resp.setHeader('Cache-Control', 'public, max-age=0');
-
-                    if (amorphicOptions.sourceMode != 'debug') {
-                        resp.end(
-                            "document.write(\"<script src='" + url.replace(/\.js/, '.cached.js') + "'></script>\");\n" +
-                            "amorphic.setApplication('" + appName + "');" +
-                            'amorphic.setSchema(' + JSON.stringify(session.getPersistorProps()) + ');' +
-                            'amorphic.setConfig(' + JSON.stringify(JSON.parse(session.getServerConfigString())) + ');' +
-                            'amorphic.setInitialMessage(' + session.getServerConnectString() + ');'
-                        );
-                    }
-                    else {
-                        resp.end(
-                            getModelSource(appName, applicationSource) +
-                            "amorphic.setApplication('" + appName + "');" +
-                            'amorphic.setSchema(' + JSON.stringify(session.getPersistorProps()) + ');' +
-                            'amorphic.setConfig(' + JSON.stringify(JSON.parse(session.getServerConfigString())) + ');' +
-                            'amorphic.setInitialMessage(' + session.getServerConnectString() + ');'
-                        );
-                    }
-
-                    req.amorphicTracking.addServerTask({name: 'Application Initialization'}, time);
-                    displayPerformance(req);
-                }
-            }).done();
-    }
 }
 
 /**
@@ -375,7 +266,7 @@ function startUpServer(preSessionInject, postSessionInject, appList, appStartLis
         .use(downloadRouter.bind(this, applicationConfig, sessions, amorphicOptions, applicationSource, applicationSourceMap, applicationPersistorProps, hostName, controllers, nonObjTemplatelogLevel, sendToLog))
         .use(connect.bodyParser())
         .use(postRouter.bind(this, applicationConfig, sessions, amorphicOptions, applicationSource, applicationSourceMap, applicationPersistorProps, hostName, controllers, nonObjTemplatelogLevel, sendToLog))
-        .use(amorphicEntry);
+        .use(amorphicEntry.bind(this, applicationSourceMap, amorphicOptions, applicationSource, applicationConfig, sessions, applicationPersistorProps, hostName, controllers, nonObjTemplatelogLevel, sendToLog));
 
     if (postSessionInject) {
         postSessionInject.call(null, app);

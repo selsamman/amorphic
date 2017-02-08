592c717eee9821c0f26e9d21d3defdfd6b5a3ec0
Sam Elsamman <sam.elsamman@gmail.com>
2017-02-08 16:03:51 -0500

    Dealt with deferred persistor/get/fetch on client and plumbed config for lazyTemplateLoad

--- client.js
+++ client.js
@@ -22,46 +22,50 @@
 RemoteObjectTemplate._injectIntoTemplate = function (template) {
     // Add persistors to foreign key references
     var schema = amorphic.schema[template.__name__] || {};
-    var props = template.getProperties();
 
-    for (var prop in props) {
-
-        var defineProperty = props[prop];
-
-        if (defineProperty.autoFetch || schema[prop]) {
-            (function () {
-                var closureProp = prop;
-                var closureDefineProperty = defineProperty;
-
-                if (!props[closureProp + 'Persistor']) {
-                    template.createProperty(closureProp + 'Persistor', {type: Object, toServer: false,
-                        value:{isFetched: !defineProperty.autoFetch, isFetching: false}});
-                }
-
-                if (!template.prototype[closureProp + 'Fetch']) {
-                    template.createProperty(closureProp + 'Fetch', {on: 'server', body: function () {}});
-                }
-
-                if (!template.prototype[closureProp + 'Get']) {
-                    template.createProperty(closureProp + 'Get', {on: 'client', body: function () {
-                        var persistor = this[closureProp + 'Persistor'];
-
-                        if ((persistor.isFetched == false) && !persistor.isFetching) {
-                            persistor.isFetching = true;
-
-                            if (closureDefineProperty.type == Array) {
-                                this[closureProp] = [];
+    template._injectProperties = function () {
+        var props = this.getProperties();
+        for (var prop in props) {
+    
+            var defineProperty = props[prop];
+    
+            if (defineProperty.autoFetch || schema[prop]) {
+                (function () {
+                    var closureProp = prop;
+                    var closureDefineProperty = defineProperty;
+    
+                    if (!props[closureProp + 'Persistor']) {
+                        this.createProperty(closureProp + 'Persistor', {type: Object, toServer: false,
+                            value:{isFetched: !defineProperty.autoFetch, isFetching: false}});
+                    }
+    
+                    if (!this.prototype[closureProp + 'Fetch']) {
+                        this.createProperty(closureProp + 'Fetch', {on: 'server', body: function () {}});
+                    }
+    
+                    if (!this.prototype[closureProp + 'Get']) {
+                        this.createProperty(closureProp + 'Get', {on: 'client', body: function () {
+                            var persistor = this[closureProp + 'Persistor'];
+    
+                            if ((persistor.isFetched == false) && !persistor.isFetching) {
+                                persistor.isFetching = true;
+    
+                                if (closureDefineProperty.type == Array) {
+                                    this[closureProp] = [];
+                                }
+    
+                                this[closureProp + 'Fetch'].call(this);
                             }
-
-                            this[closureProp + 'Fetch'].call(this);
-                        }
-
-                        return this[closureProp];
-                    }});
-                }
-            })();
+    
+                            return this[closureProp];
+                        }});
+                    }
+                })();
+            }
         }
     }
+    if (template.defineProperties)
+        template._injectProperties();
 };
 
 var module = {exports: {}};

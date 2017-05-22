c541c4fae74193cf66f64ab148cf69824b487f9e
Chris Seltzer <csmastermind@gmail.com>
2017-05-22 15:50:24 -0400

    Add __dictionary__ to amorphicStatic's definition

--- lib/utils/remoteable.ts
+++ lib/utils/remoteable.ts
@@ -17,7 +17,8 @@ export class amorphicStatic {
     static end (persistorTransaction?, logger?) : any {};
     static commit (options?) : any {};
     static createTransientObject(callback : any) : any {};
-    static __transient__ : any
+    static __transient__ : any;
+    static __dictionary__: any;
 }
 
 export function Remoteable<BC extends Constructable<{}>>(Base: BC) {

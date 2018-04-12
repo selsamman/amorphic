8c81491a5744bef3d601a4c88e1653a5c287e757
Nikhil Mashettiwar <nikhil@havenlife.com>
2018-04-12 13:45:08 -0400

    moved all amorphic-types into amorphic

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,16 @@
-export {Supertype} from 'supertype';
-export {Persistable, ContainsPersistable, Persistor} from 'persistor';
-export {Remoteable, amorphicStatic} from './lib/utils/remoteable';
-export {Bindable} from 'amorphic-bindster';
-import {Persistor} from 'persistor';
+import {Supertype} from 'supertype';
+import {Persistable, Persistor} from 'persistor';
+import {Remoteable} from './lib/utils/remoteable';
+import {Bindable} from 'amorphic-bindster';
+
+export {amorphicStatic} from './lib/utils/remoteable';
+export {ContainsPersistable} from 'persistor';
+
+export class Persistent extends Persistable(Supertype){}  // classes that have no business in any browser
+export class Serializable extends Supertype {} // classes used in APIs but are serializable
+export class AppController extends Remoteable(Bindable(Supertype)) {} // main controllers
+export class SubController extends Remoteable(Supertype) {} // controllers hanging off main controller
+export class Everything extends Persistable(Remoteable(Bindable(Supertype))) {} // Doesn't match regex
 
 // This class is for Amorphic unit tests
 export class Amorphic extends Persistor {
